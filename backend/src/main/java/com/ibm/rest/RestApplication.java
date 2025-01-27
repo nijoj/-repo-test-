cebc1ef0b495dc9a16fab8798da0c48aab54618c
NJ <nijo@ibm.com>
2025-01-27 20:39:58 +0530

    add greeting resource

--- backend/src/main/java/com/ibm/rest/RestApplication.java
+++ backend/src/main/java/com/ibm/rest/RestApplication.java
@@ -1,16 +1,28 @@
 package com.ibm.rest;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.MediaType;
 
 @ApplicationPath("/api")
 public class RestApplication extends Application {
+    // @Inject
+    // GreetingService service;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
+
+    // @GET
+    // @Produces(MediaType.TEXT_PLAIN)
+    // @Path("/greeting/{name}")
+    // public String greeting(String name) {
+    //     return service.greeting(name);
+    // }
 }
