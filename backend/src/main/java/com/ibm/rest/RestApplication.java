71d64146db5b023bb549cca98c5423df5c3b54ff
NJ <nijo@ibm.com>
2025-01-27 20:39:03 +0530

    add helloworld resource

--- backend/src/main/java/com/ibm/rest/RestApplication.java
+++ backend/src/main/java/com/ibm/rest/RestApplication.java
@@ -1,9 +1,16 @@
 package com.ibm.rest;
 
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
 
 @ApplicationPath("/api")
 public class RestApplication extends Application {
-
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
 }
