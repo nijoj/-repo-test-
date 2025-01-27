c046b2c537cf053166ba71139f4050f6f3fbe8ed
NJ <nijo@ibm.com>
2025-01-27 20:35:10 +0530

    ad footer to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <p>app works!</p>
       <Header></Header>
+      <Footer></Footer>
     </>
   )
 }
