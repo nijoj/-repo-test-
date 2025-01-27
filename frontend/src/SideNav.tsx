78538df73055cbc2a6e8b7706ab7924f7cac93d9
NJ <nijo@ibm.com>
2025-01-27 20:31:44 +0530

    add about list

--- frontend/src/SideNav.tsx
+++ frontend/src/SideNav.tsx
@@ -8,6 +8,9 @@ const SideNav = () => {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
